388f9ca63157959c3827d495cdf0e2ae7df7f4ea
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2023-10-23 10:28:29 +0530

    Update api.js

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8080/";
+const baseURL = "https://misty-dog-lapel.cyclic.app/";
 export const apiList = {
   Signup: baseURL + "api/registration/signUp",
   Signin: baseURL + "api/registration/signIn",
