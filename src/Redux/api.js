73e7d2f9d8037c4b9d7faf5ab4cf958279a3a8c0
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2023-10-30 18:53:09 +0530

    Update api.js

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,4 +1,4 @@
-const baseURL = "https://api.sirglobal.org/";
+const baseURL = "http://api.sirglobal.org/";
 export const apiList = {
   Signup: baseURL + "api/registration/signUp",
   Signin: baseURL + "api/registration/signIn",
