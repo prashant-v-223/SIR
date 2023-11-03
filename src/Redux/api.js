8b195b244b35311427b7b883df8ba1ef4fa4162b
prashant-v-223 <prashantvadhvana@gmail.com>
2023-11-03 09:16:27 +0530

    QWE

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8080/";
+const baseURL = "https://api.sirglobal.org/";
 export const apiList = {
   Signup: baseURL + "api/registration/signUp",
   Signin: baseURL + "api/registration/signIn",
